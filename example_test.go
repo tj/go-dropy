b2bdfd82c9ab6ce40fd8b94865c4a4f15f3bc4f5
TJ Holowaychuk <tj@vision-media.ca>
2015-11-28 14:05:14 -0800

    add Example

---
+++ example_test.go
@@ -0,0 +1,26 @@
+package dropy_test
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/segmentio/go-env"
+	"github.com/tj/go-dropbox"
+	"github.com/tj/go-dropy"
+)
+
+// Upload and read a file.
+func Example() {
+	token := env.MustGet("DROPBOX_ACCESS_TOKEN")
+	client := dropy.New(dropbox.New(dropbox.NewConfig(token)))
+
+	file := client.Open("/demo.txt")
+	io.Copy(file, strings.NewReader("Hello World"))
+	file.Sync()
+
+	io.Copy(os.Stdout, file)
+	// Output: Hello World
+
+	file.Close()
+}
