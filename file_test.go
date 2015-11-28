e480909a4321bf4cfa80cfcffa4b4411e0459b58
TJ Holowaychuk <tj@vision-media.ca>
2015-11-28 13:20:50 -0800

    add File tests

---
+++ file_test.go
@@ -0,0 +1,20 @@
+package dropy
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFile_Open(t *testing.T) {
+	t.Parallel()
+	c := client()
+
+	f := c.Open("/hello.txt")
+
+	b, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "whoop", string(b))
+}
